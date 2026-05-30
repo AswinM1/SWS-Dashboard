16aa618295433261051fcfcff7466139c53155b8
Aswin <117593557+AswinM1@users.noreply.github.com>
2026-05-30 14:29:42 +0530

    backend end

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 import express from "express";
 import { Server } from "socket.io";
 import { createServer } from "node:http";
+import multer from 'multer'
+
 
 const app = express();
 
+
 const server = createServer(app);
+
+const upload=new multer({
+    dest:"uploads/"
+})
 
 const io = new Server(server, {
   cors: {
@@ -18,14 +25,13 @@ io.on("connection", (socket) => {
   console.log("socket connected");
 });
 
-app.post("/files", async (req, res) => {
-  console.log("connected");
+app.post("/files",upload.array("files"),async (req, res) => {
 
-  io.emit("connectedbro", {
-    msg: "hello",
-  });
-
-  res.send("hello");
+    io.emit("new file uploaded",{
+        msg:"files uploaded",
+        file:req.files[0].originalname
+    })
+  res.send("uploaded");
 });
 
 server.listen(3000, () => {
