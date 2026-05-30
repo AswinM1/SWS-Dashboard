513bfdf0f4c8d92c01e3cc7a8b5c5d53bfddb790
Aswin <117593557+AswinM1@users.noreply.github.com>
2026-05-30 14:30:09 +0530

    frontend adjustments

--- dashboard/src/components/Fileupload.jsx
+++ dashboard/src/components/Fileupload.jsx
@@ -1,12 +1,52 @@
-import { useRef } from "react";
-
+import { useEffect, useRef, useState } from "react";
+import axios from 'axios'
+import {io} from 'socket.io-client'
 export default function App() {
+  
+  const socket=io("http://localhost:3000");
+  useEffect(()=>
+  {
+    socket.on("new file uploaded",(data)=>
+    {
+      alert(data.file)
+      
+    })
+
+  },[])
+  const handleFiles=async(selectedfiles)=>
+    {
+    const formData=new FormData()
+    selectedfiles.forEach((data)=>
+    {
+      formData.append("files",data)
+    })
+  await axios.post(
+      "http://localhost:3000/files",
+      formData
+    );
+       
+  }
+  
+
+ 
+  const[files,setFiles]=useState([]);
+  
   const inputRef = useRef();
 
+
+  const handleDrop = async(e) => {
+  e.preventDefault();
+
+  console.log(e.dataTransfer.files);
+  const selectedfiles=Array.from(e.dataTransfer.files)
+  setFiles((prev)=>[...prev,...selectedfiles])
+
+  await handleFiles(selectedfiles)
+};
   return (
     <div className="p-10">
       <h1 className="text-3xl font-bold mb-8">
-        SWS AI Document Hub
+        Random docs
       </h1>
 
       <div
@@ -14,6 +54,8 @@ export default function App() {
         onClick={() =>
           inputRef.current.click()
         }
+        onDragOver={(e)=>e.preventDefault()}
+        onDrop={handleDrop}
       >
         <h2 className="text-2xl font-semibold">
           Upload files
@@ -27,7 +69,25 @@ export default function App() {
           type="file"
           hidden
           ref={inputRef}
+          onChange={async(e)=>
+          {
+            e.preventDefault()
+            const selectedfiles=Array.from(e.target.files)
+            setFiles((prev)=>[...prev,...selectedfiles])
+            await handleFiles(selectedfiles)
+
+          }
+          }
+         
         />
+      </div>
+      <div className="mt-5">
+        {files.map((val,idx)=>
+        (
+          <div className="text-black">
+            {val.name}
+          </div>
+        ))}
       </div>
     </div>
   );
